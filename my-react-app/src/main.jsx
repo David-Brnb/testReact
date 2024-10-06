e6085af31519f0f899e13be74641f7ccda2bd794
Dafget34 <321171197@alumno.unam.mx>
2024-10-06 11:52:21 -0600

    add

--- my-react-app/src/main.jsx
+++ my-react-app/src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import SignUp from './SignUp.jsx';
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <SignUp />
   </StrictMode>,
 )
